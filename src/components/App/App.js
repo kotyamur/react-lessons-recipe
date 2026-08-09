45c50f83c537fa6737a4da459ca4ea5ef735d4b4
Kateryna Kleshch <ketrins1993@gmail.com>
2026-08-09 17:30:47 +0300

    add

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
-// import recipes from '../../recipes.json';
-// import { RecipeList } from '../RecipeList/RecipeList';
+import recipes from '../../recipes.json';
+import { RecipeList } from '../RecipeList/RecipeList';
 import { PaymentWidget } from '../PaymentWidget/PaymentWidget';
 
 export const App = () => {
   return (
     <>
-      {/* <RecipeList recipes={recipes} /> */}
+      <RecipeList recipes={recipes} />
       <PaymentWidget />
     </>
   );
